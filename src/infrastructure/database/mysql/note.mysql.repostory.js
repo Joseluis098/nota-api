e92d4179e41bb0fd25210640ae95b8c9af61dd03
Joseluis098 <joseluismamani.jlmq@gmail.com>
2026-04-19 09:14:42 -0400

    Task 2: separar modelo Sequelize del repositorio MySQL

--- src/infrastructure/database/mysql/note.mysql.repostory.js
+++ src/infrastructure/database/mysql/note.mysql.repostory.js
@@ -1,27 +1,18 @@
-import { DataTypes } from "sequelize";
-import sequelize from "./connection.js";
+import NoteModel from "./note.model.js";
 
-const NoteModel = sequelize.define("Note", {
-    title: { type: DataTypes.STRING, allowNull: false },
-    content: { type: DataTypes.TEXT, allowNull: false },
-    imageUrl: { type: DataTypes.STRING },
-    isPrivate: { type: DataTypes.BOOLEAN, defaultValue: false },
-    password: { type: DataTypes.STRING },  
-    userId: { type: DataTypes.STRING, allowNull: false }
-}, { timestamps: true });  
- 
 export default class NoteMySQLRepository {
     async save(noteEntity) {
-          const note = await NoteModel.create({
-            title: noteEntity.title,
-            content: noteEntity.content,
-            imageUrl: noteEntity.imageUrl,
+        const note = await NoteModel.create({
+            title:     noteEntity.title,
+            content:   noteEntity.content,
+            imageUrl:  noteEntity.imageUrl,
             isPrivate: noteEntity.isPrivate,
-            password: noteEntity.password,
-            userId: noteEntity.userid
+            password:  noteEntity.password,
+            userId:    noteEntity.userid
         });
         return note.toJSON();
     }
+
     async findByUserId(userId) {
         return await NoteModel.findAll({ where: { userId } });
     }
